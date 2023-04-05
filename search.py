90a9ab4999abea9ace8d272ee884b9681575f308
yellowbutton <84281387+yellowbutton@users.noreply.github.com>
2023-04-05 13:20:18 +0800

    Update search.py
    更错了重新来

--- search.py
+++ search.py
@@ -1,62 +1,58 @@
-import requests
-from bs4 import BeautifulSoup
-from urllib import parse
+import os,difflib,math,Levenshtein
+import win32ui
+import win32gui
 from icecream import ic
-
-import requests,time,random,hashlib,json
-
-def translate(i):
-    lts = str(int(time.time()*1000))
-    salt = lts + str(random.randint(0,9))
-    sign_str = 'fanyideskweb' + i + salt + 'Ygy_4c=r#e#4EX^NUGUc5'
-    m = hashlib.md5()
-    m.update(sign_str.encode())
-    sign = m.hexdigest()
-    url = 'https://fanyi.youdao.com/translate_o?smartresult=dict&smartresult=rule'
-    headers = {
-        "Referer": "https://fanyi.youdao.com/",
-        "Cookie": 'OUTFOX_SEARCH_USER_ID=-1124603977@10.108.162.139; JSESSIONID=aaamH0NjhkDAeav9d28-x; OUTFOX_SEARCH_USER_ID_NCOO=1827884489.6445506; fanyi-ad-id=305426; fanyi-ad-closed=1; ___rl__test__cookies=1649216072438',
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.84 Safari/537.36"
-    }
-
-    data = {
-        "i": i,
-        "from": "en",
-        "to": "zh",
-        "smartresult": "dict",
-        "client": "fanyideskweb",
-        "salt": salt,
-        "sign": sign,
-        "lts": lts,
-        "bv": "a0d7903aeead729d96af5ac89c04d48e",
-        "doctype": "json",
-        "version": "2.1",
-        "keyfrom": "fanyi.web",
-        "action": "FY_BY_REALTlME",
-    }
-
-    res = requests.post(url,headers=headers,data=data)
-    response = json.loads(res.text)
-
-    value = response['translateResult'][0][0]['tgt']
-    return value
-
-def searchweb(name):
-    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61 Safari/537.36 Edg/94.0.992.31'}
-    url = 'https://cn.bing.com/search?q={}'.format("what is software: "+name)
-    res = requests.get(url,headers=headers)
-    r = res.text
-    soup=BeautifulSoup(r,'html.parser')
-    jpgo=[]
-    jpgo+=soup.find_all("div",class_="b_focusTextMedium")
-    jpgo+=soup.find_all("div",class_="b_focusTextLarge")
-    if len(jpgo)==0:
+def similar(s1,s2):
+    return (Levenshtein.ratio(s1, s2))/1
+def searchmainp(path):
+    waitfslect=[]
+    path=path.replace("\\", "/")
+    name=path.split("/")
+    for f in os.listdir(path):
+        ftype=f.split(".")[-1]
+        if ftype == "exe":
+            waitfslect.append(f)
+    if len(waitfslect)==0:
         return None
-    return [jpgo[0].string,translate(jpgo[0].string)]
+    elif len(waitfslect) == 1:
+        return waitfslect[0]
+    else:
+        for i in range(0,len(waitfslect)):
+            # waitfslect[i]=[waitfslect[i],difflib.SequenceMatcher(None, waitfslect[i].split(".")[0].lower(), name[-1].lower()).quick_ratio()]
+            waitfslect[i]=[waitfslect[i],similar(waitfslect[i].split(".")[0].lower(),name[-1].lower())]
+        for i in range(0,len(waitfslect)):
+            large, small = win32gui.ExtractIconEx(path+"/"+waitfslect[i][0],0)
+            if len(large)!=0:
+                waitfslect[i][1]+=0.1
+            if len(small)!=0:
+                waitfslect[i][1]+=0.1
+            if "unins" in waitfslect[i][0] or "Unins" in waitfslect[i][0] or "inst" in waitfslect[i][0] or "Inst" in waitfslect[i][0] or "update" in waitfslect[i][0] or "Update" in waitfslect[i][0]:
+                waitfslect[i][1]+=-0.3
+            if "launch" in waitfslect[i][0] or "Launch" in waitfslect[i][0] or "win32" in waitfslect[i][0]:
+                waitfslect[i][1]+=0.3
+            # print(os.path.getsize(path+"/"+waitfslect[i][0]))
+        def s0s(lt):
+            return os.path.getsize(path+"/"+lt[0])
+        waitfslect.sort(key=s0s)
+        waitfslect[-1][1]+=0.2
+        def s1(lt):
+            return lt[1]
+        waitfslect.sort(key=s1)
+        # ic(waitfslect)
+        return waitfslect[-1][0]
 
-# print(searchweb("fiddler"))
-
-# print(parse.quote("什么是+python"))
-# print("自从20世纪90年代初Python语言诞生至今" in soup.prettify())
-# print(soup.findAll(name="div"))
-# ic("rwrl_pri" in res.text)
+def serchprofiledir(dir_path):
+    dir_path=dir_path.replace("\\", "/")
+    res = []
+    for path in os.listdir(dir_path):
+        # check if current path is a file
+        if not os.path.isfile(os.path.join(dir_path, path)):
+            res.append(path)
+    all=[]
+    for i in res:
+        s=searchmainp(dir_path+"/"+i)
+        if s != None:
+            all.append([i,searchmainp(dir_path+"/"+i)])
+    return all
+# ic(serchprofiledir(r'D:\Program Files'))
+# ic(searchmainp(r"D:\Program Files (x86)\TeamSpeak 3 Client"))
